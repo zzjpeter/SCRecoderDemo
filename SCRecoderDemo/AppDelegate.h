622c13fa900a347bfe8769906fe6d632e1a87650
zhuzhijia <zhuzhijia@lespark.us>
2019-05-17 10:03:02 +0800

    Initial Commit

---
+++ SCRecoderDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SCRecoderDemo
+//
+//  Created by 朱志佳 on 2019/5/17.
+//  Copyright © 2019 朱志佳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SCRecoderDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SCRecoderDemo
+//
+//  Created by 朱志佳 on 2019/5/17.
+//  Copyright © 2019 朱志佳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
